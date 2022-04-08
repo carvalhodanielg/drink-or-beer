2bcf0e7ed4dc4b7378e2bfc6d21d2c81b02c0700
carvalhodaniel <carvalhodanielg@gmail.com>
2022-04-08 20:59:08 -0300

    drinks feature implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 
 function App() {
 
+const [beerName, setBeerName] = useState();
+const [beerDescription, setBeerDescription] = useState();
+const [beerFood, setBeerFood] = useState();
+const [beerImage, setBeerImage] = useState();
+const [beer, setBeer] = useState();
 
-const [beer, setBeer] = useState([]);
+const [drinkName, setDrinkName] = useState();
 
 const loadBeers = () => {
 
@@ -14,33 +19,57 @@ const loadBeers = () => {
         return response.json();
       })
       .then((json)=>{
-        setBeer(json);
-       
+        setBeerName(json[0].name);
+        setBeerDescription(json[0].description)
+        setBeerFood(json[0].food_pairing[0])
+        setBeerImage(json[0].image_url)
+        setBeer(json)
+      }).catch((e)=>{
+        console.error(e)
       })
 
 }
 
 
+const loadDrinks = () => [
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then((response)=>{
+      return response.json();
+    })
+    .then((json)=>{
+      console.log(json.drinks[0])
+      setDrinkName(json.drinks[0].strDrink)
+    })
 
-  return (
+
+]
+
+
+return (
     <div className="main">
        Cervejas:
-       {console.log(beer)}
-        <button onClick={loadBeers}>Clica!</button>
+
+        <button onClick={loadBeers}>Cerveja!</button>
+        <button onClick={loadDrinks}>Drink!</button>
+
       <br/><br/>
       <div>
-        {beer[0].name}
+        {beerName}
+        {drinkName}
+
         <br/><br/>
-        {beer[0].description}
+        {beerDescription}
         <br/><br/><br/>
-        {beer[0].food_pairing[0]}
+
+        {beerFood}
+
 
         <br/><br/><br/><br/>
 
-        <img src={beer[0].image_url} width={60}></img>
+        <img src={beerImage} width={60}></img>
 
 
-        {console.log(beer[0].name)}
+
       </div>
     </div>
   );
